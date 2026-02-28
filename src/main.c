9a275e02abf256df72cc63241798e71999fabb53
yusuke <yuusuke29413@gmail.com>
2026-02-28 19:18:32 +0900

    redirect

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,7 +13,7 @@
 int main() {
   while (1) {
     printf("myshell> ");
-    fflush(stdin);
+    fflush(stdout);
     // input
     char buf[MAXPROMPTSIZE];
     fgets(buf, sizeof(buf), stdin);
@@ -33,6 +34,29 @@ int main() {
       token = strtok(NULL, " ");
     }
     argv[argv_i] = NULL;
+
+    int redirect_index = -1;
+    int redirect_no_argument_flg = 0;
+    int second_redirect_flg = 0;
+    for (int i = 0; argv[i] != NULL; ++i) {
+      if (strcmp(argv[i], ">") == 0) {
+        if (redirect_index == 1) {
+          second_redirect_flg = 1;
+          fprintf(stderr, ">: use only one >\n");
+          break;
+        }
+        if (argv[i + 1] == NULL) {
+          fprintf(stderr, ">: missing argument\n");
+          redirect_no_argument_flg = 1;
+          break;
+        }
+        redirect_index = i;
+        argv[redirect_index] = NULL;
+      }
+    }
+    if (redirect_no_argument_flg == 1 || second_redirect_flg == 1) {
+      continue;
+    }
 
     // builtin
     if (strcmp(argv[0], "cd") == 0) {
@@ -55,6 +79,19 @@ int main() {
         exit(EXIT_FAILURE);
       case 0:
         signal(SIGINT, SIG_DFL);
+        if (redirect_index != -1) {
+          int fd = open(argv[redirect_index + 1], O_WRONLY | O_CREAT | O_TRUNC,
+                        0644);
+          if (fd == -1) {
+            perror("failed to fd");
+            exit(EXIT_FAILURE);
+          }
+          if (dup2(fd, STDOUT_FILENO) == -1) {
+            perror("failed to dup2");
+            exit(EXIT_FAILURE);
+          }
+          close(fd);
+        }
         if (execvp(argv[0], argv) == -1) {
           perror("execvp failed");
           exit(EXIT_FAILURE);
