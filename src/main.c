983a9fdcd0ed003a8dcb1fefdc0a3b7c72f8f299
yusuke <yuusuke29413@gmail.com>
2026-03-05 08:20:40 +0900

    Add use parser

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 
 #include "constants.h"
+#include "parser.h"
 
 enum {
   MAXPROMPTSIZE = 1024,
@@ -31,55 +32,10 @@ int main() {
       continue;
     }
 
+    // parse
     char* argv[MAXARGSIZE];
-    int argv_i = 0;
-    char* token = strtok(buf, " ");
-    while (token != NULL) {
-      argv[argv_i] = token;
-      ++argv_i;
-      token = strtok(NULL, " ");
-    }
-    argv[argv_i] = NULL;
-
-    // TODO: 構造体にしてもいいかもしれない
-    int redirect_index = -1;
-    int redirect_no_argument_flg = 0;
-    int second_redirect_flg = 0;
-    int pipe_index = -1;
-    int pipe_no_argument_flg = 0;
-    int second_pipe_flg = 0;
-    for (int i = 0; argv[i] != NULL; ++i) {
-      if (strcmp(argv[i], ">") == 0) {
-        if (redirect_index != -1) {
-          second_redirect_flg = 1;
-          fprintf(stderr, ">: use only one time\n");
-          break;
-        }
-        if (argv[i + 1] == NULL) {
-          fprintf(stderr, ">: missing argument\n");
-          redirect_no_argument_flg = 1;
-          break;
-        }
-        redirect_index = i;
-        argv[redirect_index] = NULL;
-      } else if (strcmp(argv[i], "|") == 0) {
-        if (pipe_index != -1) {
-          second_pipe_flg = 1;
-          fprintf(stderr, "|: use only one time\n");
-          break;
-        }
-        if (argv[i + 1] == NULL) {
-          fprintf(stderr, "|: missing argument\n");
-          pipe_no_argument_flg = 1;
-          break;
-        }
-        pipe_index = i;
-      }
-    }
-    if (redirect_no_argument_flg == 1 || second_redirect_flg == 1 ||
-        pipe_no_argument_flg == 1 || second_pipe_flg == 1) {
-      continue;
-    }
+    Tokenize(argv, buf);
+    ParseResult parse_result = ParsePrompt(argv);
 
     // builtin
     if (strcmp(argv[0], "cd") == 0) {
@@ -94,23 +50,7 @@ int main() {
     }
 
     // fork for pipe
-    if (pipe_index != -1) {
-      char* left_argv[MAXARGSIZE];
-      for (int i = 0; i < pipe_index; ++i) {
-        left_argv[i] = argv[i];
-      }
-      left_argv[pipe_index] = NULL;
-
-      char* right_argv[MAXARGSIZE];
-      int idx = 0;
-      while (1) {
-        right_argv[idx] = argv[pipe_index + 1 + idx];
-        if (argv[pipe_index + 1 + idx] == NULL) {
-          break;
-        }
-        ++idx;
-      }
-
+    if (parse_result.pipe_index != -1) {
       int pipefd[2];
       if (pipe(pipefd) == -1) {
         perror("failed to pipe\n");
@@ -127,7 +67,7 @@ int main() {
           close(pipefd[0]);
           dup2(pipefd[1], STDOUT_FILENO);
           close(pipefd[1]);
-          execvp(left_argv[0], left_argv);
+          execvp(parse_result.left_argv[0], parse_result.left_argv);
         default:
           pid_t pid2 = fork();
           switch (pid2) {
@@ -139,7 +79,7 @@ int main() {
               close(pipefd[1]);
               dup2(pipefd[0], STDIN_FILENO);
               close(pipefd[0]);
-              execvp(right_argv[0], right_argv);
+              execvp(parse_result.right_argv[0], parse_result.right_argv);
             default:
               close(pipefd[0]);
               close(pipefd[1]);
@@ -160,9 +100,9 @@ int main() {
         exit(EXIT_FAILURE);
       case 0:
         signal(SIGINT, SIG_DFL);
-        if (redirect_index != -1) {
-          int fd = open(argv[redirect_index + 1], O_WRONLY | O_CREAT | O_TRUNC,
-                        0644);
+        if (parse_result.redirect_index != -1) {
+          int fd = open(argv[parse_result.redirect_index + 1],
+                        O_WRONLY | O_CREAT | O_TRUNC, 0644);
           if (fd == -1) {
             perror("failed to fd");
             exit(EXIT_FAILURE);
